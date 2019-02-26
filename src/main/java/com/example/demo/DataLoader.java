2e1d15af2d2ec8abb8bb12ef4e48b10931d0f7be
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-26 17:58:38 -0500

    Lesson 3.03

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements CommandLineRunner {
 
     @Autowired
